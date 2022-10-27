70b19d859c09f3421ca6b87e731891c534688033
Vibhor-Kumar2903 <81754839+Vibhor-Kumar2903@users.noreply.github.com>
2022-10-27 20:49:04 +0530

    27/10 - 2

--- features/steps/orangehrmsteps.py
+++ features/steps/orangehrmsteps.py
@@ -1,10 +1,16 @@
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
-@given('Launch chrome brower')
+
+@given('launch chrome brower')
 def launchBrower(context):
-    context.driver = webdriver.chrome("config/chromedriver.exe")
+    # context.driver = webdriver.Chrome()
+    # context.driver = webdriver.Chrome(executable_path='C:\\Users\Vibhor\Desktop\Driver\chromedriver.exe')
+    context.driver = webdriver.Chrome()
 
 
 @when('open orangeHrm home page')
@@ -14,8 +20,9 @@ def openHomePage(context):
 
 @then('verify that the logo present on page')
 def verifyLogo(context):
-    logo = context.driver.find_element_by_xpath("").is_displayed()
-    assert logo is True
+    context.driver.implicitly_wait(20)
+    logo_status = context.driver.find_element(By.XPATH, "//div[@class='orangehrm-login-branding']//img").is_displayed()
+    assert logo_status is True
 
 
 @then('close browser')
